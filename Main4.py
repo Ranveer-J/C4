eab2f069b524e5a6fcd92c4c8de058420c16c97d
Ranveer-J <83578974+Ranveer-J@users.noreply.github.com>
2024-11-25 17:53:50 +0000

    Add files via upload

---
+++ Main4.py
@@ -0,0 +1,17 @@
+num = 23
+print("Table of 23")
+
+for i in range(1,11):
+    mul = num*i
+    print("23 * %d = %d" % (i,mul)) 
+
+
+
+num1 = 5
+for i in range(0,num1):
+    
+    for j in range(0,i+1):
+        print("*" ,end=" ")
+
+    print("\n")
+
